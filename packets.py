caa649fc4d17e6ea84fadf143a928fa8dd693250
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2019-05-19 17:02:40 +0300

    Add packets generator

---
+++ packets.py
@@ -0,0 +1,19 @@
+import random
+
+
+class PacketGenerator:
+    def __init__(self, seed=None, is_different=True):
+        self.is_different = is_different
+        self.seed = seed
+        self.random = random
+        self.reset_seed()
+
+    def generate_packet(self, packet_size):
+        if not self.is_different:
+            self.reset_seed()
+        byte_sequence = bytearray(self.random.getrandbits(8) for _ in range(packet_size))
+        return byte_sequence
+
+    def reset_seed(self):
+        self.random.seed(self.seed)
+
